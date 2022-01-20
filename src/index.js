7f8b4485254f22675f56e9b225b184d4fca3e05c
Leeyo Rapirap <leorapirap@gmail.com>
2022-01-20 16:44:49 +0800

    Create a new TaskList component

--- src/index.js
+++ src/index.js
@@ -4,20 +4,30 @@ import "./index.css";
 
 // my first component
 
+function TaskList() {
+    return(
+    	<ul>
+	    	<li className="tasks-item">Follow Edukasyon.ph on Facebook</li>
+	    	<li className="tasks-item">Follow AWS Siklab Pilipinas on Facebook</li>
+	    	<li className="tasks-item">Follow Zuitt Coding Bootcamp on Facebook</li>
+    	</ul>
+    );
+}
+
 const App = () => {
 	const paraStyle = {
-		"marginBottom":"15px"
+		"marginBottom":"15px",
+		"color":"orangered"
+	}
+	const blue = {
+		"color":"lightblue"
 	}
     return (
     	<div className="app">
         <header className="app-header">
-            <h1>My Amazing ToDo-List App</h1>
+            <h1 style={blue}>My Amazing ToDo-List App</h1>
             <p style={paraStyle}>The most simple and amazing todo-list React App.</p>
-            <ul>
-            	<li className="tasks-item">Follow Edukasyon.ph on Facebook</li>
-            	<li className="tasks-item">Follow AWS Siklab Pilipinas on Facebook</li>
-            	<li className="tasks-item">Follow Zuitt Coding Bootcamp on Facebook</li>
-            </ul>
+            <TaskList />
         </header>
         </div>
     )
